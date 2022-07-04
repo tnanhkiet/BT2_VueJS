f16f58bb996c4a88a01106491ed0c6c06b8b8610
tnanhkiet <tnanhkiet99@gmail.com>
2022-07-04 22:48:56 +0700

    commit

--- main.js
+++ main.js
@@ -1,46 +1,73 @@
-import './components/product.js'
+import "./components/product.js";
 
 new Vue({
-	el: "#app",
-    data: {
-        search: '',
-        perPage: 10,
-        pageNumber: 0,
-        productData: [],
-        productChecked: [],
+  el: "#app",
+  data: {
+    search: "",
+    perPage: 10,
+    pageNumber: 0,
+    productData: [],
+    productChecked: [],
+  },
+  created: function () {
+    fetch("assets/data/product.json")
+      .then((response) => response.json())
+      .then((data) => (this.productData = data));
+  },
+  computed: {
+    currentPageItems() {
+      return this.productData.slice(
+        this.pageNumber * this.perPage,
+        this.pageNumber * this.perPage + 1 + this.perPage
+      );
     },
-    created:  function() {
-        fetch('assets/data/product.json')
-            .then(response => response.json())
-            .then(data => (this.productData = data))
+    filteredLists() {
+      return this.currentPageItems.filter((item) => {
+        return (
+          item.name.toLowerCase().includes(this.search.toLowerCase()) ||
+          item.id.toLowerCase().includes(this.search.toLowerCase())
+        );
+      });
     },
-    computed:{
-        currentPageItems() {
-            return this.productData.slice(this.pageNumber * this.perPage, this.pageNumber * this.perPage + 1 + this.perPage)
-        },
-        filteredLists() {
-            return this.currentPageItems.filter(item => {
-                return item.name.toLowerCase().includes(this.search.toLowerCase()) || item.id.toLowerCase().includes(this.search.toLowerCase())
-            })
-        },
-        handleSelected() {
-            if(this.productChecked.length > 0) {
-                return true
-            } else {
-                return false
-                
-            }
-        }
+    handleSelected() {
+      if (this.productChecked.length > 0) {
+        return true;
+      } else {
+        return false;
+      }
     },
-    methods: {
-        prev() {
-            this.pageNumber--
-        },
-        next() {
-            this.pageNumber++
-        },
-        handleSave() {
-            localStorage.setItem('selectedProducts', JSON.stringify(this.productChecked))
-        }
-    }
-})
+  },
+  mounted() {
+    this.productChecked = this.getProductsLocal()
+  },
+  methods: {
+    sortedList(e) {
+      if (e.target.value == "1") {
+        this.currentPageItems.sort((n1, n2) => {
+          let a = n1.name.toLowerCase();
+          let b = n2.name.toLowerCase();
+          return a === b ? 0 : a > b ? 1 : -1;
+        });
+        console.log("A-Z");
+      } else {
+        this.currentPageItems.reverse();
+        console.log("Z-A");
+      }
+    },
+    prev() {
+      this.pageNumber--;
+    },
+    next() {
+      this.pageNumber++;
+    },
+    handleSave() {
+      localStorage.setItem(
+        "selectedProducts",
+        JSON.stringify(this.productChecked)
+      );
+    },
+    getProductsLocal() {
+      return JSON.parse(localStorage.getItem("selectedProducts")) || [];
+    },
+  },
+});
